0d6e22d767e63f3282e5dfcbf165637b4d66d371
Ranvijay Singh <ranvijsysingh2012@gmail.com>
2023-04-20 20:57:07 +0530

    add project structure

--- src/main/java/com/springboot/elasticsearch/ElasticSearchApplication.java
+++ src/main/java/com/springboot/elasticsearch/ElasticSearchApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ElasticSearchApplication {
 
 	public static void main(String[] args) {
-		System.out.println();
 		SpringApplication.run(ElasticSearchApplication.class, args);
 	}
 
